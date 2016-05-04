f6dbb47a12a8810ce2c764292a5c731c5d31ba6f
Stanislav Nikolov <stanislav@bluecreation.com>
2016-05-04 13:45:15 +0100

    Initial Commit

---
+++ MelodySmart2 Sample AppTests/MelodySmart2_Sample_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  MelodySmart2_Sample_AppTests.swift
+//  MelodySmart2 Sample AppTests
+//
+//  Created by Stanislav Nikolov on 04/05/2016.
+//  Copyright © 2016 BlueCreation. All rights reserved.
+//
+
+import XCTest
+@testable import MelodySmart2_Sample_App
+
+class MelodySmart2_Sample_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
